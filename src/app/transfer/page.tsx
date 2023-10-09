7ee0dd06fb38585e504a1f4b5bdf66626a931546
Russel <emkil.russel@gmail.com>
2023-10-09 18:39:50 +0500

    remove redundant logic from start page

--- src/app/transfer/page.tsx
+++ src/app/transfer/page.tsx
@@ -1,12 +1,25 @@
 'use client';
 import '@app/globals.css'
 import { useRouter } from 'next/navigation'
+import { useState, useEffect } from 'react'
+import secureLocalStorage  from  "react-secure-storage";
 
-export default function OnboardingStartPage() {
+export default function TransferPage() {
+  const [mnemonic, setMnemonic] = useState<string>("")
+
+  useEffect(() => {
+    const mnemonic = secureLocalStorage.getItem("wallet")
+
+    if (typeof mnemonic === 'string') {
+        setMnemonic(mnemonic as string)
+    }
+  }, [setMnemonic])
+
   const router = useRouter()
 
   return (
     <div className="h-screen flex flex-col justify-center items-center">
+      <label>{mnemonic}</label>
       <button className="btn btn-blue mt-4" onClick={() => router.push('/onboarding/create-wallet')}>
           Create Wallet
       </button>
