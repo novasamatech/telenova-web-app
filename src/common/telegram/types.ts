eae91ae4a73ea2bca1dac284a02d32bce52aa5c4
sokolova-an <dev.anastasis@gmail.com>
2023-11-28 17:46:21 +0100

    add common variables

--- src/common/telegram/types.ts
+++ src/common/telegram/types.ts
@@ -1,17 +1,66 @@
-import {HexString} from '@common/types';
+import { HexString } from '@common/types';
 
 export type WebApp = {
-    sendData: (data: HexString) => void;
+  sendData: (data: HexString) => void;
 };
 
 export type Telegram = {
-    WebApp: WebApp;
+  WebApp: WebApp;
 };
 
 export interface ITelegram {
-    completeOnboarding: (publicKey: HexString) => void;
+  completeOnboarding: (publicKey: HexString) => void;
 }
 
 export interface ITelegramMessageFactory {
-    prepareWalletCreationData: (publicKey: HexString) => HexString | null;
+  prepareWalletCreationData: (publicKey: HexString) => HexString | null;
+}
+
+// tg types.ts
+export interface ITelegramUser {
+  id: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  language_code: string;
+}
+
+export interface IWebApp {
+  initData: string;
+  initDataUnsafe: {
+    query_id: string;
+    user: ITelegramUser;
+    auth_date: string;
+    hash: string;
+  };
+  version: string;
+  platform: string;
+  colorScheme: string;
+  themeParams: {
+    link_color: string;
+    button_color: string;
+    button_text_color: string;
+    secondary_bg_color: string;
+    hint_color: string;
+    bg_color: string;
+    text_color: string;
+  };
+  isExpanded: boolean;
+  viewportHeight: number;
+  viewportStableHeight: number;
+  isClosingConfirmationEnabled: boolean;
+  headerColor: string;
+  backgroundColor: string;
+  BackButton: {
+    isVisible: boolean;
+  };
+  MainButton: {
+    text: string;
+    color: string;
+    textColor: string;
+    isVisible: boolean;
+    isProgressVisible: boolean;
+    isActive: boolean;
+  };
+  HapticFeedback: any;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+const { nextui } = require('@nextui-org/react');
+
+import fontSizes from './tw-config-consts/font-sizes';
+import colors from './tw-config-consts/colors';
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/screens/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/screens/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx',
   ],
   theme: {
     extend: {
+      fontSize: fontSizes,
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        ...colors,
       },
     },
   },
-  plugins: [],
-}
-export default config
+  darkMode: 'class',
+  plugins: [nextui()],
+};
+export default config;
