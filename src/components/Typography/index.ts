0d998f1763d3f99ce72a61d56171fd2ebec0fdb4
sokolova-an <dev.anastasis@gmail.com>
2023-11-30 10:54:30 +0100

    add typography components

---
+++ src/components/Typography/index.ts
@@ -0,0 +1,11 @@
+import { BodyText } from './components/BodyText';
+import { FootnoteText } from './components/FootnoteText';
+import { TitleText } from './components/TitleText';
+import { CaptionText } from './components/CaptionText';
+import { HeadlineText } from './components/HeadlineText';
+import { LargeTitleText } from './components/LargeTitleText';
+import { LabelText } from './components/LabelText';
+import { HelpText } from './components/HelpText';
+import TextBase from './common/TextBase';
+
+export { TextBase, FootnoteText, BodyText, TitleText, CaptionText, LargeTitleText, HeadlineText, LabelText, HelpText };
