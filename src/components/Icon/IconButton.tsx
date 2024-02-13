91de5cbb749104de3c17a5ccac8e056533c83c4c
sokolova-an <dev.anastasis@gmail.com>
2024-02-13 16:54:49 +0100

    delete unused

--- src/components/Icon/IconButton.tsx
+++ src/components/Icon/IconButton.tsx
@@ -12,17 +12,17 @@ type Props = {
 
 const IconButton = ({ iconName, onClick, size = 40, text = '' }: Props) => {
   return (
-    <div className="grid">
-      <Button
-        radius="full"
-        variant="shadow"
-        className="bg-white h-full w-full p-1 gap-4 justify-start border border-border-neutral shadow-button"
-        onClick={onClick}
-      >
+    <Button
+      radius="full"
+      variant="shadow"
+      className="bg-white h-full w-full p-1 gap-4 justify-start border border-border-neutral shadow-button"
+      onClick={onClick}
+    >
+      <span className="w-[40px]">
         <Icon name={iconName} size={size} className="text-[--tg-theme-button-color]" />
-        <LabelText as="span">{text}</LabelText>
-      </Button>
-    </div>
+      </span>
+      <LabelText as="span">{text}</LabelText>
+    </Button>
   );
 };
 
--- src/components/Icon/types.ts
+++ src/components/Icon/types.ts
@@ -4,7 +4,6 @@ import KSM from '@common/assets/svg/assets/KSM.svg';
 import Settings from '@common/assets/svg/settings.svg';
 import Send from '@common/assets/svg/Send.svg';
 import Receive from '@common/assets/svg/Receive.svg';
-import Buy from '@common/assets/svg/Buy.svg';
 import Address from '@common/assets/svg/Address.svg';
 import User from '@common/assets/svg/User.svg';
 import ChevronForward from '@common/assets/svg/chevron-forward.svg';
@@ -31,7 +30,6 @@ const AllIcons = {
   Settings,
   Send,
   Receive,
-  Buy,
   Address,
   User,
   ChevronForward,
@@ -51,34 +49,6 @@ const AllIcons = {
   Pen,
   Blind,
 };
-
-// const AllIcons = {
-//   DOT: 'assets/DOT.svg',
-//   WND: 'assets/WND.svg',
-//   KSM: 'assets/KSM.svg',
-//   settings: 'settings.svg',
-//   send: 'Send.svg',
-//   receive: 'Receive.svg',
-//   buy: 'Buy.svg',
-//   address: 'Address.svg',
-//   user: 'User.svg',
-//   chevronForward: 'chevron-forward.svg',
-//   arrowBold: 'arrow-bold.svg',
-//   scanQr: 'scan.svg',
-//   gift: 'gift.svg',
-//   backup: 'backup.svg',
-//   currency: 'currency.svg',
-//   language: 'language.svg',
-//   novaWallet: 'nova-wallet.svg',
-//   telegram: 'Telegram.svg',
-//   twitter: 'X-twitter.svg',
-//   youtube: 'Youtube.svg',
-//   welcome: 'welcome.svg',
-//   present: 'present.svg',
-//   userBlock: 'user-block.svg',
-//   pen: 'pen.svg',
-//   blind: 'blind.svg',
-// } as const;
 
 export type IconNames = keyof typeof AllIcons;
 
