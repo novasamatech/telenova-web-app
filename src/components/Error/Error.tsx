eb72f93d22d7af78868da81a280c868c4e939601
sokolova-an <dev.anastasis@gmail.com>
2024-02-19 16:22:38 +0100

    remove reset button

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,28 +1,13 @@
 import Image from 'next/image';
-import { Button } from '@nextui-org/react';
 
-import { resetWallet } from '@/common/wallet';
-import { BodyText, MediumTitle, TitleText } from '@/components';
-import { Paths } from '@/common/routing';
+import { MediumTitle, TitleText } from '@/components';
 
 const Error = () => {
-  const handleReset = () => {
-    resetWallet(true);
-    window.location.replace(Paths.ONBOARDING_IMPORT_WALLET);
-  };
-
   return (
     <div className="h-screen p-4 w-full flex flex-col items-center justify-center gap-3">
       <Image src="/images/error.svg" alt="error" width={100} height={100} />
       <TitleText className="mt-3">Unexpected Error</TitleText>
       <MediumTitle>Something went wrong, try again later</MediumTitle>
-      <BodyText>
-        If you encounter this error again, reopening the application or resetting your wallet locally by clicking the
-        button below. Your wallet data is secure during this process.
-      </BodyText>
-      <Button className="font-manrope" onClick={handleReset}>
-        Reset Wallet
-      </Button>
     </div>
   );
 };
