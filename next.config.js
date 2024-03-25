1e257765f25119fdbf3c9eb44712919bcba5d8a9
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2024-03-25 18:11:44 +0300

    Feat: Remove ENV from the build process. (#115)
    * fix env assignment
    
    * Remove ENV from the build
    
    * remove redundant env variables
    
    * update required secrets
    
    * update env list
    
    * test deploy
    
    * test
    
    * return prod value back

--- next.config.js
+++ next.config.js
@@ -18,4 +18,14 @@ nextConfig.webpack = (config, context) => {
   return config;
 };
 
+nextConfig.env = {
+  NEXT_PUBLIC_BOT_API_URL: process.env.NEXT_PUBLIC_BOT_API_URL,
+  NEXT_PUBLIC_BOT_ADDRESS: process.env.NEXT_PUBLIC_BOT_ADDRESS,
+  NEXT_PUBLIC_WEB_APP_ADDRESS: process.env.NEXT_PUBLIC_WEB_APP_ADDRESS,
+  NEXT_PUBLIC_WEB_APP_URL: process.env.NEXT_PUBLIC_WEB_APP_URL,
+  NEXT_PUBLIC_WIDGET_SECRET: process.env.NEXT_PUBLIC_WIDGET_SECRET,
+  NEXT_PUBLIC_SECURE_LOCAL_STORAGE_DISABLED_KEYS: process.env.NEXT_PUBLIC_SECURE_LOCAL_STORAGE_DISABLED_KEYS,
+  DOCKER_TAGS: process.env.DOCKER_TAGS,
+};
+
 module.exports = nextConfig;
