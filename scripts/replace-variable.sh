df27dcab737fd5baef90d7c5fca1e9c10add189b
Stepan Lavrentev <lawrentievsv@gmail.com>
2024-05-23 12:21:04 +0300

    replace variable with shell

---
+++ scripts/replace-variable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# replace-variables.sh
+
+VARIABLES=$(node -e "console.log(Object.keys(require('./next.config.js').env).join(' '))")
+
+for VAR in $VARIABLES; do
+    if [ -z "$(eval echo \$$VAR)" ]; then
+        echo "$VAR is not set. Please set it and rerun the script."
+        exit 1
+    fi
+done
+
+find /app/public /app/.next -type f -name "*.js" |
+while read file; do
+    for VAR in $VARIABLES; do
+        sed -i "s|$VAR|$(eval echo \$$VAR)|g" "$file"
+    done
+done
