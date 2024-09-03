14288f9b18e2e398d2639f97fe6a6755de730400
Yaroslav Grachev <tuulwq@gmail.com>
2024-09-03 16:44:20 +0300

    chore: unnecessary console log

--- app/common/extrinsicService/ExtrinsicProvider.tsx
+++ app/common/extrinsicService/ExtrinsicProvider.tsx
@@ -3,7 +3,7 @@ import type { EstimateFeeParams, SubmitExtrinsicParams } from './types';
 import { type PropsWithChildren, createContext, useContext, useMemo } from 'react';
 
 import { type Hash } from '@polkadot/types/interfaces';
-import { type BN, BN_ZERO } from '@polkadot/util';
+import { type BN } from '@polkadot/util';
 
 import { FAKE_ACCOUNT_ID } from '@/shared/helpers';
 
@@ -20,16 +20,10 @@ export const ExtrinsicProvider = ({ children }: PropsWithChildren) => {
   const { prepareExtrinsic } = useExtrinsicService();
 
   async function estimateFee({ chainId, transaction, signOptions, options }: EstimateFeeParams): Promise<BN> {
-    try {
-      const extrinsic = await prepareExtrinsic<'promise'>(chainId, transaction, options);
-      const paymentInfo = await extrinsic.paymentInfo(FAKE_ACCOUNT_ID, signOptions);
+    const extrinsic = await prepareExtrinsic<'promise'>(chainId, transaction, options);
+    const paymentInfo = await extrinsic.paymentInfo(FAKE_ACCOUNT_ID, signOptions);
 
-      return paymentInfo.partialFee.toBn();
-    } catch (e) {
-      console.log('=== e', e);
-    }
-
-    return BN_ZERO;
+    return paymentInfo.partialFee.toBn();
   }
 
   async function submitExtrinsic({
