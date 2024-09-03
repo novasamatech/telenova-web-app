a2bd30be2bd553b9b7dde1d6f9a717c138a37db4
Yaroslav Grachev <tuulwq@gmail.com>
2024-09-03 15:20:30 +0300

    chore: legacy clipboard

--- app/shared/helpers/browser.ts
+++ app/shared/helpers/browser.ts
@@ -5,35 +5,41 @@
  * @param text Value to be copied
  */
 export function copyToClipboard(id: string, text: string) {
-  const fallback = (id: string) => {
-    const textElement = document.getElementById(id);
-    const range = document.createRange();
-    const selection = window.getSelection();
-
-    if (!textElement || !selection) return;
-
-    selection.removeAllRanges();
-    range.selectNodeContents(textElement);
-    selection.addRange(range);
-
-    try {
-      document.execCommand('copy');
-    } catch (error) {
-      console.error('Oops, unable to copy:', error);
-    }
-
-    selection.removeAllRanges();
-  };
-
   if (navigator.clipboard) {
     navigator.clipboard.writeText(text).catch(error => {
       console.info('Could not copy to clipboard, trying fallback', error);
 
-      fallback(id);
+      copyToClipboardLegacy(id);
     });
   } else {
-    fallback(id);
+    copyToClipboardLegacy(id);
   }
+}
+
+/**
+ * Copy to the clipboard that relies upon deprecated
+ * document.execCommand('copy')
+ *
+ * @param id Element id to apply a fallback
+ */
+function copyToClipboardLegacy(id: string) {
+  const textElement = document.getElementById(id);
+  const range = document.createRange();
+  const selection = window.getSelection();
+
+  if (!textElement || !selection) return;
+
+  selection.removeAllRanges();
+  range.selectNodeContents(textElement);
+  selection.addRange(range);
+
+  try {
+    document.execCommand('copy');
+  } catch (error) {
+    console.error('Oops, unable to copy:', error);
+  }
+
+  selection.removeAllRanges();
 }
 
 /**
