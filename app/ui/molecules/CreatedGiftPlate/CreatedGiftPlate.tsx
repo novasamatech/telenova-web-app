d5ffc7cb0f9d55304db771bc7aaa6057bb0bc3d0
Yaroslav Grachev <tuulwq@gmail.com>
2024-10-18 16:52:42 +0300

    feat: setting image

--- app/ui/molecules/CreatedGiftPlate/CreatedGiftPlate.tsx
+++ app/ui/molecules/CreatedGiftPlate/CreatedGiftPlate.tsx
@@ -5,12 +5,16 @@ import { useUnit } from 'effector-react';
 import { $path } from 'remix-routes';
 
 import { networkModel } from '@/models/network';
-import { getGifts } from '@/shared/helpers';
+import { cnTw, getGifts } from '@/shared/helpers';
 import { useGifts } from '@/shared/hooks';
 import { type Gift } from '@/types/substrate';
-import { BigTitle, BodyText, Icon, Plate, Shimmering } from '@/ui/atoms';
+import { BodyText, Icon, MediumTitle, Plate, Shimmering } from '@/ui/atoms';
 
-export const CreatedGiftPlate = () => {
+type Props = {
+  className?: string;
+};
+
+export const CreatedGiftPlate = ({ className }: Props) => {
   const { getGiftsState } = useGifts();
 
   const connections = useUnit(networkModel.$connections);
@@ -27,20 +31,20 @@ export const CreatedGiftPlate = () => {
   }, [connections]);
 
   return (
-    <Plate className="mt-4 h-[90px] w-full rounded-3xl border-1 border-border-neutral active:bg-bg-item-pressed">
-      <Link to={$path('/gifts')} className="grid w-full grid-cols-[auto,1fr,auto] items-center gap-4">
-        <Icon name="Present" size={60} />
-        <div className="grid">
-          <BigTitle align="left">Created Gifts</BigTitle>
-          {unclaimed ? (
-            <BodyText align="left" className="text-text-hint">
-              {unclaimed.length ? `Unclaimed: ${unclaimed.length}` : 'All your gifts were claimed'}
-            </BodyText>
-          ) : (
-            <Shimmering width={100} height={20} />
-          )}
+    <Plate
+      className={cnTw(
+        'w-full rounded-3xl border-1 border-border-neutral px-4 py-2.5 active:bg-bg-item-pressed',
+        className,
+      )}
+    >
+      <Link to={$path('/gifts')} className="grid h-10 w-full grid-cols-[auto,1fr,auto] items-center gap-x-2">
+        <Icon name="Gift" size={25} className="text-text-link" />
+        <div className="flex items-center gap-x-1">
+          <MediumTitle className="font-medium">Created Gifts</MediumTitle>
+          {!unclaimed && <Shimmering width={20} height={20} />}
+          {unclaimed && unclaimed?.length > 0 && <BodyText className="text-text-hint">{unclaimed.length}</BodyText>}
         </div>
-        <Icon name="ArrowBold" className="h-10 w-10 self-center" />
+        <Icon name="ChevronForward" size={24} className="self-center" />
       </Link>
     </Plate>
   );
